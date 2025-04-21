d88ba8cb1329917091ef1deb0c9c208e43a8e94d
Md Ariful Islam <proariful@gmail.com>
2025-04-22 00:14:43 +0600

    unwanted file delete

--- web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ImageUploadController;
-
-// Route for image upload
-Route::post('/upload', [ImageUploadController::class, 'upload']);
